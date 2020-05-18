4dfd1c09d5e4e8a714bf27d31a49546e5370ca57
aacebo <44811138+aacebo@users.noreply.github.com>
2020-05-18 15:56:38 -0400

    add user screen and icons

--- src/main/MainScreen.tsx
+++ src/main/MainScreen.tsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Feather } from '@expo/vector-icons';
 
 import PullRequestsScreen from './pull-requests/PullRequestsScreen';
+import UserScreen from './user/UserScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -11,12 +13,28 @@ export default class MainScreen extends Component {
     return (
       <NavigationContainer>
         <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: (props) => {
+              let icon = '';
+
+              if (route.name === 'Pull Requests') {
+                icon = 'git-pull-request';
+              } else if (route.name === 'User') {
+                icon = 'user';
+              }
+
+              return <Feather name={icon} size={30} color={props.color} />;
+            },
+          })}
           tabBarOptions={{
             activeTintColor: 'tomato',
             inactiveTintColor: 'gray',
+            showLabel: false,
+            style: { backgroundColor: 'black' },
           }}
         >
           <Tab.Screen name="Pull Requests" component={PullRequestsScreen} />
+          <Tab.Screen name="User" component={UserScreen} />
         </Tab.Navigator>
       </NavigationContainer>
     );
